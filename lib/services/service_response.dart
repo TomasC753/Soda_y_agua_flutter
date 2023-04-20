10267f6bde16311fad2968837eb25eb3e338a35f
TomasC753 <tcatalano159@gmail.com>
2023-04-20 01:21:42 -0300

    api service improvement

--- lib/services/service_response.dart
+++ lib/services/service_response.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+enum OperationStatus { loading, loadingMore, success, empty, error }
 
 class ServiceResponse<T> {
   late Rx<T> data;
-  RxStatus status;
+  // RxStatus status;
+  Rxn<OperationStatus> status;
   Future<T> Function() getterFunction;
 
   RxString errorMessage = ''.obs;
@@ -10,19 +14,40 @@ class ServiceResponse<T> {
   ServiceResponse(
       {required this.data, required this.status, required this.getterFunction});
 
-  void getData() async {
-    status.isLoading;
+  Future<void> getData() async {
     try {
+      status.value = OperationStatus.loading;
       T dataObtained = await getterFunction();
       data.value = dataObtained;
-      if (dataObtained != null || dataObtained != List.empty()) {
-        status.isSuccess;
+      if (dataObtained != null && dataObtained != List.empty()) {
+        // status = RxStatus.success();
+        status.value = OperationStatus.success;
       } else {
-        status.isEmpty;
+        // status.isEmpty;
+        // status = RxStatus.empty();
+        status.value = OperationStatus.empty;
       }
     } catch (e) {
-      status.isError;
+      // status.isError;
+      // status = RxStatus.error();
+      status.value = OperationStatus.error;
       errorMessage.value = e.toString();
     }
+  }
+
+  Widget? returnContentWhen(
+      {Widget? onSuccess,
+      Widget? onLoading,
+      Widget? onError,
+      Widget? onEmpty,
+      Widget? onLoadingMore}) {
+    Map<OperationStatus, Widget?> results = {
+      OperationStatus.success: onSuccess,
+      OperationStatus.loading: onLoading,
+      OperationStatus.empty: onEmpty,
+      OperationStatus.error: onError,
+      OperationStatus.loadingMore: onLoadingMore
+    };
+    return results[status];
   }
 }
