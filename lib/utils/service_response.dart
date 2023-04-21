d1bdfe30767897b22ae1e52ff28c82053b7734df
TomasC753 <tcatalano159@gmail.com>
2023-04-21 01:46:44 -0300

    Modify Response Service

--- lib/utils/service_response.dart
+++ lib/utils/service_response.dart
@@ -6,49 +6,6 @@ part 'response_list.dart';
 
 enum OperationStatus { loading, loadingMore, success, empty, error }
 
-// class ServiceResponse<T> {
-//   late Rx<T> data;
-//   Rxn<OperationStatus> status;
-//   Future<T> Function() getterFunction;
-
-//   RxString errorMessage = ''.obs;
-
-//   ServiceResponse(
-//       {required this.data, required this.status, required this.getterFunction});
-
-// Future<void> getData() async {
-//   try {
-//     status.value = OperationStatus.loading;
-//     T dataObtained = await getterFunction();
-//     data.value = dataObtained;
-//     if (dataObtained != null && dataObtained != List.empty()) {
-//       status.value = OperationStatus.success;
-//     } else {
-//       status.value = OperationStatus.empty;
-//     }
-//   } catch (e) {
-//     status.value = OperationStatus.error;
-//     errorMessage.value = e.toString();
-//   }
-// }
-
-//   Widget? returnContentWhen(
-//       {Widget? onSuccess,
-//       Widget? onLoading,
-//       Widget? onError,
-//       Widget? onEmpty,
-//       Widget? onLoadingMore}) {
-//     Map<OperationStatus, Widget?> results = {
-//       OperationStatus.success: onSuccess,
-//       OperationStatus.loading: onLoading,
-//       OperationStatus.empty: onEmpty,
-//       OperationStatus.error: onError,
-//       OperationStatus.loadingMore: onLoadingMore
-//     };
-//     return results[status];
-//   }
-// }
-
 class IServiceResponse<T> {
   Rxn<OperationStatus> status;
   Future<T> Function() getterFunction;
--- test/services/ClientService_test.dart
+++ test/services/ClientService_test.dart
@@ -1,4 +1,1 @@
-test('crudFunctionalities property should not be null', () {
-final clientService = ClientService();
-expect(clientService.crudFunctionalities, isNotNull);
-});
+
